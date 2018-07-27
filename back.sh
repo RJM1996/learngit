e38f577e1dcbfc0febf6b02e55bfd4e1635dcd26
rugu <rjm96@foxmail.com>
2018-07-27 12:34:54 +0800

    12:34

--- back.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mycommand=`rsync -a --exclude 'Linux/http/mysql' /home/admin/learngit /mnt/hgfs/CentOS7`
-$mycommand
-
-curtime=`date +%Y-%m-%d--%H:%M`
-echo $curtime > /home/admin/learngit/time.txt
