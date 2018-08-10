babebd5ae3b44952623851a8520e81c333a9a977
rugu <rjm96@foxmail.com>
2018-08-10 23:33:01 +0800

    2018年 08月 10日 星期五 23:33:01 CST

--- Study/test_code/day_test/03_Test/test.cpp
+++ Study/test_code/day_test/03_Test/test.cpp
@@ -6,7 +6,7 @@ class A
 {
 public:
     virtual void Function()
-    // Function(int _y)
+        // Function(int _y)
     {
         // this->x = _y;
         // cout << "I can run, y: " << _y <<  endl;
@@ -19,9 +19,9 @@ private:
 class Father
 {
 public:
-    Father(int _y)
-        :x(_y)
-    {}
+    // Father(int _y)
+    //     :x(_y)
+    // {}
 
     virtual void print()
     {
@@ -65,41 +65,69 @@ typedef void(*func)(void);
 
 void test()
 {
-    // Father* ptr01 = new Father();
+    // Father* ptr01 = new Father;
     // Father* ptr02 = new Child01;
     // Father* ptr03 = new Child02;
     // ptr01->print();
     // ptr02->print();
     // ptr03->print();
-    Father f(100);
-    int* vptr = (int*)(&f);
-    cout << "虚函数指针的地址是: " << vptr << endl;
 
-    func vfunc = (func)*((int*)*(int*)(&f));
-    cout << "第一个虚函数的地址为: " << (int*)*(int*)(&f) << endl;
-    cout << "通过地址, 调用虚函数: Father::print(): " ;
-    vfunc();
+    Father f;
+    Child01 c1;
+    Child02 c2;
+    Father& r1 = f;
+    Father& r2 = c1;
+    Father& r3 = c2;
+    r1.print();
+    r2.print();
+    r3.print();
 
-    func vfunc01 = (func)* ((int*)*((int*)(&f))+2);
-    cout << "第二个虚函数的地址为: " << ((int*)(*((int*)&f)) + 2) << endl;
-    cout << "通过地址, 调用虚函数: Father::set_x(): " ;
-    vfunc01();
+    // Father f(100);
+    // int* vptr = (int*)(&f);
+    // cout << "虚函数指针的地址是: " << vptr << endl;
 
-    func vfunc02 = (func)* ((int*)*((int*)(&f))+4);
-    cout << "第三个虚函数的地址为: " << ((int*)(*((int*)&f))+4) << endl;
-    cout << "通过地址, 调用虚函数: Father::~Father(): " ;
-    vfunc02();
+    // func vfunc = (func)*(int*)*(int*)(&f);
+    // cout << "第一个虚函数的地址为: " << (int*)*(int*)(&f) << endl;
+    // cout << "通过地址, 调用虚函数: Father::print(): " ;
+    // vfunc();
+
+    // func vfunc01 = (func)* ((int*)*(int*)(&f)+2);
+    // cout << "第二个虚函数的地址为: " << ((int*)*(int*)(&f)+2) << endl;
+    // cout << "通过地址, 调用虚函数: Father::set_x(): " ;
+    // vfunc01();
+
+    // func vfunc02 = (func)* ((int*)*((int*)(&f))+4);
+    // cout << "第三个虚函数的地址为: " << ((int*)(*((int*)&f))+4) << endl;
+    // cout << "通过地址, 调用虚函数: Father::~Father(): " ;
+    // vfunc02();
 }
+
+
+class ClassA
+{
+public:
+    virtual void FunctionA(){};
+};
+class ClassB
+{
+public:
+    virtual void FunctionB(){};
+};
+class ClassC : public ClassA,public ClassB
+{
+public:
+};
+
 
 
 int main()
 {
     // A a;
     // A *pa = &a;
-    // // A *pa = NULL;
+    // // pa = NULL;
     // pa->Function(); // 相当于 Function(pa);
-    
-    test();
+
+    // test();
 
     // int x = 0;
     // int* a = &x;
@@ -107,4 +135,11 @@ int main()
     // cout << a << endl;
     // a++;
     // cout << a << endl;
+    ClassC a;
+    ClassA* pA=&a;
+    ClassB* pB=&a;
+    ClassC* pC=&a;
+    cout << pA << endl;
+    cout << pB << endl;
+    cout << pC << endl;
 }
