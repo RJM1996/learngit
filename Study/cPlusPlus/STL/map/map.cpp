7f6d7ddb9510adbe59dff70d4f97d2659f916c53
rugu <rjm96@foxmail.com>
2018-08-12 20:29:23 +0800

    2018年 08月 12日 星期日 20:29:23 CST

--- Study/cPlusPlus/STL/map/map.cpp
+++ Study/cPlusPlus/STL/map/map.cpp
@@ -10,7 +10,7 @@ void printContainer(container& s)
     typename container::const_iterator it = s.begin();
     // typename container::reverse_iterator it = s.rbegin();
     // while(it != s.end())
-    while(it != s.rend())
+    while(it != s.end())
     {
         cout << it->first << " => " << it->second << endl;
         ++it;
@@ -18,15 +18,15 @@ void printContainer(container& s)
     cout << endl;
 }
 
-// C++11 cbegin cend
-    template<class container>
-void print(container& s)
-{
-    for(auto it = s.cbegin(); it != s.cend(); it++)
-    {
-        cout << it->first << " => " << it->second << endl;
-    }
-}
+// // C++11 cbegin cend
+// template<class container>
+// void print(container& s)
+// {
+//     for(auto it = s.cbegin(); it != s.cend(); it++)
+//     {
+//         cout << it->first << " => " << it->second << endl;
+//     }
+// }
 
 void TestMap01()
 {
@@ -45,35 +45,168 @@ void TestMap01()
     map<int, char> third(first.begin(), first.end());
 
     // 打印 map
-    // printContainer(first);
-    print(second);
+    printContainer(first);
+    // print(second);
+
+    if(!first.empty())
+    {
+        cout << first.size() << endl;
+    }
+
+    cout << first[0] << endl;
+    // cout << first.at(5) << endl; // 会检查 key 的合法性, 抛异常
 }
 
-int main1()
+void TestMap02()
 {
-    TestMap01();
+    // 插入
+    map<int, char> mymap;
+    mymap.insert(pair<int, char>(1, 'x'));
+    mymap.insert(pair<int, char>(2, 'y'));
+    mymap.insert(pair<int, char>(3, 'z'));
+    printContainer(mymap);
+    pair<map<int, char>::iterator, bool> ret;
+    ret = mymap.insert(pair<int, char>(3, 'v'));
+    if(ret.second == false)
+    {
+        cout << "3 已经存在, 对应的 val => " << ret.first->second << endl;
+    }
+
+    map<int, char>::iterator it = mymap.begin();
+    mymap.insert(it, pair<int, char>(4, 'b'));
+    mymap.insert(it, pair<int, char>(5, 'n'));
+    printContainer(mymap);
+
+    map<int, char> othermap;
+    othermap.insert(mymap.begin(), mymap.find(4)); // 从mymap的第一个到 4 的前一个
+    printContainer(othermap);
+
+    // 删除
+    mymap.erase(2); // 指定 key 删除
+    printContainer(mymap);
+    it = mymap.find(5);
+    mymap.erase(it); // 根据 find 返回的迭代器删除
+    printContainer(mymap);
+    it = mymap.find(1);
+    mymap.erase(it, mymap.end()); // 删除一个区间
+    printContainer(mymap);
+    cout << "mymap size: " << mymap.size() << endl;
+    cout << "othermap size: " << othermap.size() << endl;
+    othermap.clear();
+    cout << "othermap size: " << othermap.size() << endl;
+}
+
+void TestMap03()
+{
+    // key_compare key_comp() const;
+    map<int, char> mymap;
+    mymap[0] = 'a';
+    mymap[1] = 'b';
+    mymap[2] = 'c';
+    mymap[3] = 'd';
+    map<int, char>::key_compare mycmp = mymap.key_comp();
+    
+    // int low = mymap.begin()->first;
+    int high = mymap.rbegin()->first;
+
+    map<int, char>::iterator it = mymap.begin();
+    do 
+    {
+        cout << it->first << " => " << it->second << endl;
+    } while(mycmp((*it++).first, high));
+    cout << endl;
+}
+
+void TestMap04()
+{
+    map<int, char> mymap;
+    mymap[1] = 'x';
+    mymap[2] = 'y';
+    mymap[3] = 'z';
+    mymap[4] = 'v';
+    mymap[5] = 'm';
+    mymap[6] = 'n';
+    mymap[7] = 'b';
+    printContainer(mymap);
+    cout << "mymap.find(2)->second: " << mymap.find(2)->second << endl;
+    map<int, char>:: iterator it = mymap.find(3);
+    if(it != mymap.end())
+    {
+        mymap.erase(it);
+    }
+    printContainer(mymap);
+    it = mymap.find(9); // 返回 map::end
+    // mymap.erase(it); 运行会出错
+    
+    int n;
+    for(n=1; n<10; n++)
+    {
+        if(mymap.count(n) == 1)
+        {
+            cout << "key: " << n << endl;
+        }
+        else
+        {
+            cout << "key: 不存在 " << endl;
+        }
+    }
+
+    map<int, char>::iterator itlow, itup;
+    // itlow = mymap.lower_bound(2);
+    // itup = mymap.lower_bound(6);
+    itlow = mymap.upper_bound(2);
+    itup = mymap.upper_bound(6);
+    mymap.erase(itlow, itup);
+    printContainer(mymap);
+}
+
+void TestMap_WaterMargin()
+{
+
+    map <string, string> mymap;
+    mymap.insert(pair<string, string>("宋江", "及时雨"));
+    mymap.insert(pair<string, string>("吴用", "智多星"));
+    mymap.insert(make_pair("李逵", "黑旋风"));
+    mymap.insert(make_pair("武松", "行者"));
+    mymap.insert(make_pair("鲁智深", "花和尚"));
+    cout << "Map size: " << mymap.size() << endl;
+    // 插入 key 存在, val 不同的元素
+    mymap.insert(make_pair("李逵", "铁牛"));
+    cout << "Map size: " << mymap.size() << endl;
+    printContainer(mymap);
+
+    // 通过 key 查找 val
+    cout << "武松: " << mymap["武松"] << endl;
+
+    // 修改 key 对应的 val
+    cout << "李逵: " << mymap["李逵"] << endl;
+    mymap["李逵"] = "铁牛";
+    cout << "李逵: " << mymap["李逵"] << endl;
+
+    // 通过 [] 访问 key 对应的 val 时, 如果 key 不存在, 会创建一个默认 val 的键值对
+    cout << "武大郎: " << mymap["武大郎"] << endl;
+    cout << "Map size: " << mymap.size() << endl;
+    cout << "武大郎: " << mymap["武大郎"] << endl; // val 默认为空
+
+    // 删除
+    mymap.erase(mymap.find("武大郎"));
+    cout << "Map size: " << mymap.size() << endl;
+
+    // 清空
+    mymap.clear();
+    cout << "Map size: " << mymap.size() << endl;
+}
+
+int main()
+{
+    // TestMap01();
+    // TestMap02();
+    // TestMap03();
+    // TestMap04();
+    TestMap_WaterMargin();
     return 0;
 }
 
 
 
 
-#include <iostream>
-#include <map>
-
-int main ()
-{
-    std::map<char,int> mymap;
-
-    mymap['b'] = 100;
-    mymap['a'] = 200;
-    mymap['c'] = 300;
-
-    // print content:
-    std::cout << "mymap contains:";
-    for (auto it = mymap.cbegin(); it != mymap.cend(); ++it)
-        std::cout << " [" << (*it).first << ':' << (*it).second << ']';
-    std::cout << '\n';
-
-    return 0;
-}
