4f352c55da0bd742e16d5918eeca6827421de55c
rjm <rjm96@foxmail.com>
2018-08-15 19:51:42 +0800

    2018年 08月 15日 星期三 19:51:42 CST

---
+++ Study/python/crawler/test01.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+'''
+用 BeautifulSoup4 爬虫库 爬小说
+'''
+
+import urllib2 # Python 标准库 提供 url 操作方法
+import re 
+from bs4 import BeautifulSoup # Python 三方库, 提供响应html内容的操作
+
